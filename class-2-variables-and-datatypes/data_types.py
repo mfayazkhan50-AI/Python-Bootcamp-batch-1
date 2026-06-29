e9c71a4d0575cc4be8f1fc0130c12b4542011e1c
Muhammad Fayaz Khan <muhammadfayazkhan50@gmail.com>
2026-06-29 13:18:15 +0500

    Add files via upload

---
+++ class-2-variables-and-datatypes/data_types.py
@@ -0,0 +1,48 @@
+# """
+# 1. string
+# 2. int
+# 3. float
+# 4. bool
+# """
+
+
+# # we use type(variable) to identify the variable type
+
+
+# # print("my name is", name)
+
+# # print(f"My name is {name}")
+
+
+# 'My name is Yasir'
+
+# # methods of string
+
+# name = "YASIR ALI SHAH IS FROM NAURANG"
+
+# '''
+# 1. title
+# 2. upper
+# 3. lower
+# '''
+# formated_name = name.lower()
+
+# print(f"before editing {name}")
+# print(f"after editing {formated_name}")
+
+
+
+
+# Floats
+
+# integers
+
+marks1 = 120    # int
+percentage = 7.9   # float
+name = 'hasan'  # string
+passed = True # bool
+
+print(f'the type of marks1: {type(marks1)}')
+print(f'the type of percentage: {type(percentage)}')
+print(f'the type of name: {type(name)}')
+print(f'the type of passed: {type(passed)}')
